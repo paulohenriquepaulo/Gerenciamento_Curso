c459356f2ef9c7cd74fa48331f0949894e9398ed
Paulo-dev-98 <paulobg8@gmail.com>
2022-07-29 21:27:40 -0300

    commit nome

--- br.com.gerenciamento.curso/src/main/java/com/gerenciamento/curso/curso/model/Aluno.java
+++ br.com.gerenciamento.curso/src/main/java/com/gerenciamento/curso/curso/model/Aluno.java
@@ -1,4 +1,15 @@
 package com.gerenciamento.curso.curso.model;
 
 public class Aluno {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
