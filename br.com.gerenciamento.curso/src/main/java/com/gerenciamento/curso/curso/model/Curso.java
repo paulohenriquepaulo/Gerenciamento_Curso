87c235ced3a4a69a3c34f14456389aef95c6fd26
paulohenriquepaulo <p.henrique461@gmail.com>
2022-07-30 13:36:59 -0300

    Refatoração do codigo

--- br.com.gerenciamento.curso/src/main/java/com/gerenciamento/curso/curso/model/Curso.java
+++ br.com.gerenciamento.curso/src/main/java/com/gerenciamento/curso/curso/model/Curso.java
@@ -1,7 +1,6 @@
 package com.gerenciamento.curso.curso.model;
 
 import com.gerenciamento.curso.curso.model.enums.Processo;
-import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,7 +9,6 @@ import javax.persistence.ManyToOne;
 import java.time.LocalDate;
 
 @Entity
-@Data
 public class Curso {
 
     @Id
@@ -30,5 +28,59 @@ public class Curso {
     @JoinColumn(name = "aluno_id")
     private Aluno aluno;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeCurso() {
+        return nomeCurso;
+    }
+
+    public void setNomeCurso(String nomeCurso) {
+        this.nomeCurso = nomeCurso;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getPrevisaoConclusao() {
+        return previsaoConclusao;
+    }
+
+    public void setPrevisaoConclusao(LocalDate previsaoConclusao) {
+        this.previsaoConclusao = previsaoConclusao;
+    }
+
+    public Processo getProcesso() {
+        return processo;
+    }
+
+    public void setProcesso(Processo processo) {
+        this.processo = processo;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
 }
